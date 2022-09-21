56fc68a36339fcd529730a94574dcc25f682c6d8
muhandasa <adisataofeekabiodun@gmail.com>
2022-09-21 05:15:48 -0700

    pycode

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax".split()
-print(str[5] + " " + str[6] + " " + str[12] + " " + str[0])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
